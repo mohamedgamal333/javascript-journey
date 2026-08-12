8245927eb7489ae9ebfa32a9ab2d8f66acd13772
Mohamed Gamal Goda <100122197+mohamedgamal333@users.noreply.github.com>
2026-08-12 17:12:54 +0300

    feat: add variables examples

---
+++ 01-fundamentals/01-variables/examples.js
@@ -0,0 +1,45 @@
+// Variables
+
+let age = 25;
+
+console.log(age);
+
+age = 24;
+
+console.log(age);
+
+
+// const
+
+const name = "Mohamed";
+const country = "Egypt";
+
+console.log(name);
+console.log(country);
+
+
+// var
+
+var score = 100;
+
+console.log(score);
+
+score = 150;
+
+console.log(score);
+
+
+// Hoisting with var
+
+console.log(testVar);
+
+var testVar = 10;
+
+
+// let and const with TDZ
+
+// console.log(testLet);
+// let testLet = 10;
+
+// console.log(testConst);
+// const testConst = 10;
